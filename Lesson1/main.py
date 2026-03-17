e57fc267d3cfb4770d57c7adfd35305c7e0d6ac1
tumana777 <tumana991@gmail.com>
2026-03-17 22:33:22 +0400

    add lecture 1

---
+++ Lesson1/main.py
@@ -0,0 +1,67 @@
+# first_name  = "Otar"
+# lastName = 'Tumanishvili'
+# age = 34
+# lecturer = True
+# student = False
+# height = 1.75
+
+# print(first_name, end='>>>')
+# print(lastName)
+
+# print(first_name, lastName, sep='/')
+
+# print(first_name + lastName)
+
+# test = '85'
+# age = 34
+
+# print(int(test) + age)
+
+# s = test + str(age)
+#
+# print(s)
+#
+# print(type(s))
+
+
+# print(type(False))
+
+# name = input("Enter your name: ")
+#
+# print("Hello", name, end="!")
+
+# num1 = input("Please enter a number: ")
+# num2 = input("Please enter another number: ")
+#
+# print(num1 + num2)
+
+
+
+# num1 = int(input("Please enter a number: "))
+# num2 = int(input("Please enter another number: "))
+#
+# print("Hello world")
+#
+# print(num1 + num2)
+
+
+# a = 5
+# b = 6
+#
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print(a ** b)
+
+
+# print(type(30 / 5))
+
+
+
+
+
+
+
