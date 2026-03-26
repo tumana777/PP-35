881c5b57595fade4263afd02de3e6f7191703840
tumana777 <tumana991@gmail.com>
2026-03-26 21:41:02 +0400

    add lecture 3

---
+++ Lesson 3/main.py
@@ -0,0 +1,144 @@
+# while True:
+#     print("Hello World")
+
+# num = 1
+#
+# while num <= 10:
+#     print(num)
+#     num += 1
+#
+#
+# print("End of program")
+
+# num = 1
+#
+# while num <= 10:
+#     print(num)
+#     num += 1
+# else:
+#     print("num is greater than 10")
+#
+#
+# print("End of program")
+
+# num = 1
+#
+# while num <= 10:
+#     print(num)
+#     num += 1
+#
+#     if num == 5:
+#         break
+# else:
+#     print("num is greater than 10")
+#
+#
+# print("End of program")
+
+
+# password = "test"
+# attempts = 3
+#
+#
+# while attempts > 0:
+#     user_password = input("Enter password: ")
+#
+#     if user_password == password:
+#         print("Login successful")
+#         break
+#
+#     if user_password == "":
+#         print("blank password is not allowed")
+#         continue
+#
+#     attempts -= 1
+#     print(f"Incorrect password. You have {attempts} attempts remaining.")
+# else:
+#     print("your account has been locked")
+#
+# print("End of program")
+
+
+# password = "test"
+# attempts = 3
+#
+# while True:
+#     user_password = input("Enter password: ")
+#
+#     if user_password == password:
+#         print("Login successful")
+#         break
+#
+#     if user_password == "":
+#         print("blank password is not allowed")
+#         continue
+#
+#     attempts -= 1
+#     print(f"Incorrect password. You have {attempts} attempts remaining.")
+#
+#     if attempts == 0:
+#         print("your account has been locked")
+#         break
+#
+# print("End of program")
+
+
+# number = 1
+#
+# while number < 6:
+#     print("Hello World")
+#     number += 1
+
+# name = "tumanishvili"
+
+# iterator = iter(name)
+#
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+
+
+# for letter in name:
+#     print(letter)
+
+
+# for char in "Hello World":
+#     print(char)
+
+# 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+# for i in range(10):
+#     print(i)
+
+# for i in range(5, 10):
+#     print(i)
+
+# for i in range(1, 11, 2):
+#     print(i)
+
+# for i in range(11, 1, -3):
+#     print(i)
+
+# for _ in range(5):
+#     print("Hello World")
+
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         print(f"i = {i}, j = {j}")
+
+# import random
+
+# print(random.randint(1, 10))
+# print(random.randrange(1, 10))
+# print(random.random())
+
+# from random import randint
+#
+# print(randint(1, 10))
+
+
+
+
+
+
+
