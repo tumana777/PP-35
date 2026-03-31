77ecd7856f0e670f6abb590eb91caa12a2605a46
tumana777 <tumana991@gmail.com>
2026-03-31 22:18:24 +0400

    add lecture 4

---
+++ Lesson 4/main.py
@@ -0,0 +1,116 @@
+# text = "Hello World"
+#
+# counter = 0
+#
+# for _ in text:
+#     counter += 1
+#
+# print(counter)
+
+# text = "I love Python"
+#
+# print(len(text))
+
+# print(text[0])
+# print(text[1])
+# print(text[8])
+# print(text[12])
+# print(text[15])
+
+
+# print(text[2:8])
+# print(text[0:8])
+# print(text[:8])
+
+# print(text[5:])
+
+# text = "I love Python"
+
+# print(text[len(text) - 1])
+# print(text[-1])
+# print(text[-3])
+
+# print(text[2:8])
+# print(text[2:9:2])
+# print(text[::2])
+# print(text[::-1])
+
+# print(text[8:2:-1])
+
+# text1 = "Hello "
+# text2 = "World "
+
+# print(text1 + text2)
+# print((text1 + text2) * 5)
+# print(text1 * 4)
+
+
+# text = "Otar"
+
+# print(text.upper())
+
+# text = text.upper()
+
+# print(text.lower())
+
+# user_input = input("Enter your name: ").lower()
+#
+# if user_input == text.lower():
+#     print("Hello Otar")
+# else:
+#     print("Hello stranger")
+
+
+# text = " otar tumanishvili "
+# print(text.capitalize())
+# print(text.title())
+# print(text.count('a'))
+# print(text.index('t'))
+# print(text.index('w'))
+# print(text.find('w'))
+
+# print(text.strip('o i'))
+
+# text += "wer"
+#
+# print(text)
+
+# text = "Hello World, I love Python"
+
+# print(text.split())
+
+# print(text.replace("Python", "Programming"))
+
+# text = text.replace("Python", "Programming")
+#
+# print(text)
+
+
+# name = "Otar"
+# age = 35
+
+# print(f"Hello {name}")
+
+# print("Hello %s" % name)
+
+# print("Hello {}! I am {} years old".format(name, age))
+
+# name = "Otar"
+# encoded_name = name.encode()
+#
+# print(len(name))
+# print(len(encoded_name))
+
+# print(encoded_name)
+# print(type(encoded_name))
+
+# name = "ოთარ"
+# encoded_name = name.encode()
+#
+# print(encoded_name.decode("utf-8"))
+
+# print(len(name))
+# print(len(encoded_name))
+
+# print(encoded_name)
+# print(type(encoded_name))
