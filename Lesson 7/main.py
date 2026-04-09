3a103c3e10bbb8db02a74c30bf74ee61a6b43f84
tumana777 <tumana991@gmail.com>
2026-04-09 22:27:10 +0400

    add lecture 7

---
+++ Lesson 7/main.py
@@ -0,0 +1,177 @@
+empty_tuple = ()
+
+# print(type(empty_tuple))
+
+# my_tuple = (1, 2, 3, 4, 5)
+
+# print(my_tuple)
+
+# print(my_tuple[5])
+# print(type(my_tuple))
+
+# s = ("hello",)
+#
+# print(type(s))
+
+# my_tuple = ("Hello", True, 5, 3.14, [2, 5, 6], {"name": "Otar"}, 5)
+
+# print(my_tuple[3])
+# my_tuple[3] = 3.14159265359
+# print(my_tuple[4][1])
+
+# my_tuple[4] = [2, 9, 6]
+
+# my_tuple[4][1] = 9
+#
+# print(my_tuple[4])
+
+# my_tuple[5]["age"] = 25
+#
+# print(my_tuple[5])
+
+
+# my_tuple = ("Hello", True, 5, 3.14, [2, 5, 6], {"name": "Otar"}, 5)
+
+# for item in my_tuple:
+#     print(item)
+
+# lst = list(my_tuple)
+#
+# lst[1] = False
+#
+# my_tuple = tuple(lst)
+#
+# print(my_tuple)
+
+
+# my_tuple = tuple("hello")
+#
+# print(my_tuple)
+
+# print(tuple([1, 2, 3]))
+
+# name = "Mariam"
+
+# for char in name:
+#     print(char)
+#
+# my_dict = {
+#     "name": "Otar",
+#     "age": 25
+# }
+#
+# print(tuple(my_dict))
+
+# a, b, *c = 1, 2, 6
+#
+# print(a)
+# print(b)
+# print(c)
+
+# *a, b, c = 1, 2, 6, 5
+#
+# print(a)
+# print(b)
+# print(c)
+
+# a, *b, c = 1, 3, 5, 8, 6
+#
+# print(a)
+# print(b)
+# print(c)
+
+
+# my_tuple = ("Hello", True, 5, 3.14, [2, 5, 6], {"name": "Otar"}, 5)
+#
+# my_tuple1 = (1, 2, 3, 4, 5)
+#
+# new_tuple = my_tuple + my_tuple1
+
+# print(new_tuple)
+
+# print(my_tuple * 3)
+
+# print(6 in my_tuple)
+
+# print(my_tuple.count(5))
+# print(my_tuple.index("Hello"))
+
+
+# lst = ["Alex", "Otar", "Nino", "Mary", "Alex"]
+
+# my_set = {"Alex", "Otar", "Nino", "Mary", "Alex"}
+
+# print(my_set)
+
+# print(len(my_set))
+
+# for item in my_set:
+#     print(item)
+
+# my_lst = list(my_set)
+
+# my_set = set("Hello World")
+
+# my_set = {1, 3.14, True, "Hello", (1, 2, 3)}
+#
+# print(my_set)
+
+# my_set = set()
+#
+# print((type(my_set)))
+
+# lst = ["Alex", "Otar", "Nino", "Mary", "Alex"]
+#
+# my_set = set(lst)
+
+# print(my_set)
+
+# my_set1 = {1, -35, 100, 37}
+
+# my_set1 = {2, 1, 3, 4, 5}
+# my_set2 = {4, 5, 6, 7, 8}
+
+# print(my_set1.add(100))
+# my_set1.remove(1)
+
+# my_set1 = {"Otar", "Nino", "Mary", "Alex"}
+#
+# popped = my_set1.pop()
+
+# print(popped)
+
+# my_set1 = {2, 1, 3, 4, 5}
+# my_set2 = {4, 5, 6, 7, 8}
+
+# print(my_set1.difference(my_set2))
+# print(my_set1 - my_set2)
+
+# print(my_set1.union(my_set2))
+# print(my_set1 | my_set2)
+
+# print(my_set1.intersection(my_set2))
+# print(my_set1 & my_set2)
+
+# print(my_set1.issubset(my_set2))
+# print(my_set1 <= my_set2)
+
+# print(my_set1.symmetric_difference(my_set2))
+# print(my_set1 ^ my_set2)
+
+# my_set1 = {2, 1, 3, 4, 5, 3, 4}
+# frozen_set1 = frozenset(my_set1)
+#
+# print(frozen_set1)
+
+
+
+
+
+
+
+
+
+
+
+
+
