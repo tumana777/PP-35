291e05295f86d1ec785adf19c67e4d102021d297
tumana777 <tumana991@gmail.com>
2026-05-05 22:35:14 +0400

    add lecture 14

---
+++ Lesson 14/main.py
@@ -0,0 +1,77 @@
+# a = int(5)
+# name = "Otar"
+#
+# print(type(a))
+# print(type(name))
+
+# class Student:
+#     status = True
+#     pay = 1000
+
+# student = Student()
+# student1 = Student()
+#
+# print(student.status)
+# print(student1.status)
+# print(student.pay)
+# print(student1.pay)
+
+# student.status = False
+
+# print(student.status)
+# print(student1.status)
+
+
+# class Student:
+#     status = True
+#     pay = 1000
+#     total_students = 0
+#
+#     def __init__(self, first_name, last_name, age):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.age = age
+#         Student.total_students += 1
+#
+#     def get_email(self):
+#         return f"{self.first_name}.{self.last_name}@gmail.com"
+
+# print(Student.total_students)
+
+# student1 = Student("otar", "tumanishvili", 35)
+# student2 = Student("alex", "ninidze", 21)
+#
+# print(student1)
+# print(student2)
+
+# print(type(student1))
+# print(type(student2))
+
+# print(Student.total_students)
+#
+# print(student1.get_email())
+# print(student2.get_email())
+
+# print(student1.first_name)
+# print(student1.last_name)
+# print(student1.age)
+# print(student1.status)
+# print(student1.pay)
+# print()
+# print(student2.first_name)
+# print(student2.last_name)
+# print(student2.age)
+# print(student2.status)
+# print(student2.pay)
+
+
+
+
+
+
+
+
+
+
+
+
